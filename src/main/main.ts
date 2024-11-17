81fcc115b8acb53fb7b3b9207c356ea42ad9c6b8
Omer Sadeh <omer.sadeh@protonmail.com>
2024-11-17 11:49:58 +0200

    Loader (#1)
    * added a loader window waiting for react & python to load.
    * restored OS conventions
    
    ---------

--- src/main/main.ts
+++ src/main/main.ts
@@ -13,10 +13,10 @@ import { app, BrowserWindow, shell, ipcMain, dialog } from 'electron';
 import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
 import MenuBuilder from './menu';
-import { resolveHtmlPath } from './util';
+import { getAssetPath, resolveHtmlPath } from './util';
 import { spawn } from 'child_process';
 import fs from "fs";
-import { getPort } from "get-port-please";
+import { getPort } from 'scotty-beam-me-up';
 
 class AppUpdater {
   constructor() {
@@ -27,6 +27,9 @@ class AppUpdater {
 }
 
 let mainWindow: BrowserWindow | null = null;
+let loaderWindow: BrowserWindow | null = null;
+let pyproc: any = null;
+let pyport: number | null = null;
 
 if (process.env.NODE_ENV === 'production') {
   const sourceMapSupport = require('source-map-support');
@@ -53,18 +56,32 @@ const installExtensions = async () => {
     .catch(console.log);
 };
 
-const createWindow = async () => {
+const createLoaderWindow = async () => {
+  loaderWindow = new BrowserWindow({
+    width: 200,
+    height: 200,
+    icon: getAssetPath('icon.png'),
+    transparent: true,
+    frame: false,
+    titleBarStyle: 'hidden',
+    movable: true,
+    resizable: false,
+    focusable: false,
+    webPreferences: {
+      devTools: false
+    },
+  });
+
+  loaderWindow.setWindowButtonVisibility(false)
+  loaderWindow.loadFile(getAssetPath('loader/loader.html'));
+  loaderWindow.center();
+  loaderWindow.show();
+};
+
+const createMainWindow = async () => {
   if (isDebug) {
     await installExtensions();
   }
-
-  const RESOURCES_PATH = app.isPackaged
-    ? path.join(process.resourcesPath, 'assets')
-    : path.join(__dirname, '../../assets');
-
-  const getAssetPath = (...paths: string[]): string => {
-    return path.join(RESOURCES_PATH, ...paths);
-  };
 
   mainWindow = new BrowserWindow({
     show: false,
@@ -79,16 +96,17 @@ const createWindow = async () => {
   });
 
   mainWindow.loadURL(resolveHtmlPath('index.html'));
-
   mainWindow.on('ready-to-show', () => {
     if (!mainWindow) {
       throw new Error('"mainWindow" is not defined');
     }
+    loaderWindow?.close();
     if (process.env.START_MINIMIZED) {
-      mainWindow.minimize();
+      mainWindow?.minimize();
     } else {
-      mainWindow.show();
+      mainWindow?.show();
     }
+    mainWindow?.center();
   });
 
   mainWindow.on('closed', () => {
@@ -109,51 +127,91 @@ const createWindow = async () => {
   new AppUpdater();
 };
 
+const startPythonBackend = async () => {
+  pyport = await getPort({ port: 5000 });
+  ipcMain.on('ipc-port', async (event, arg) => {
+    event.returnValue = pyport;
+  });
+
+  pyproc = null;
+  let pythonPath = process.env.NODE_ENV !== 'development' ? path.join(__dirname, '../../..', 'pythondist/app/app') : "backend/app.py"
+
+  if (fs.existsSync(pythonPath)) {
+    if (process.env.NODE_ENV === 'development') {
+      pyproc = spawn(`python3 ` + pythonPath + ` ${pyport}`, {
+        detached: false,
+        shell: true,
+        stdio: 'inherit'
+      });
+    } else {
+      const serverCmd = process.platform === 'win32' ? 'start ' + pythonPath + '.exe' : pythonPath;
+      pyproc = spawn(serverCmd + ` ${pyport}`, { detached: false, shell: true, stdio: 'pipe' });
+    }
+
+    if (pyproc === null || pyproc == undefined) {
+      dialog.showErrorBox("Error", "Failed to start Python backend.");
+      app.quit();
+    }
+  } else {
+    dialog.showErrorBox("Error", "Python backend not found.");
+    app.quit();
+  }
+};
+
+const testPythonBackend = async () => {
+  let tries = 10;
+  let response = null;
+  while (response === null && tries > 0) {
+    try {
+      response = await fetch(`http://localhost:${pyport}/check_initialization`);
+    } catch (err) {
+      tries--;
+      await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+  }
+
+  if (response == null || response.status !== 200) {
+    console.log("Failed to connect to Python backend: " + response);
+    dialog.showErrorBox("Error", "Error while loading application. Please try again.");
+    app.quit();
+  } else {
+    let res = await response.json();
+    if (res.up) {
+      console.log("Python backend started successfully.");
+    } else {
+      console.log("Failed to connect to Python backend: " + res.message);
+      dialog.showErrorBox("Error", "Error while loading application. Please try again.");
+      app.quit();
+    }
+  }
+};
+
 /**
  * Add event listeners...
  */
 
 app.whenReady()
   .then(async () => {
-      let availablePort = await getPort({ port: 5000 });
-      ipcMain.on('ipc-port', async (event, arg) => {
-          event.returnValue = availablePort;
-      });
+    await createLoaderWindow();
+    await startPythonBackend().then(testPythonBackend);
+    createMainWindow();
 
-      let pyproc = null;
-      let pythonPath = process.env.NODE_ENV !== 'development' ? path.join(__dirname, '../../..', 'pythondist/app/app') : "backend/app.py"
+    app.on('activate', () => {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (mainWindow === null) createMainWindow();
+    });
 
-      if (fs.existsSync(pythonPath)) {
-          if (process.env.NODE_ENV === 'development') {
-              pyproc = spawn(`python3 ` + pythonPath + ` ${availablePort}`, {
-                  detached: false,
-                  shell: true,
-                  stdio: 'inherit'
-              });
-          } else {
-              const serverCmd = process.platform === 'win32' ? 'start ' + pythonPath + '.exe' : pythonPath;
-              pyproc = spawn(serverCmd + ` ${availablePort}`, { detached: false, shell: true, stdio: 'pipe' });
-          }
-
-          if (pyproc === null || pyproc == undefined) {
-              dialog.showErrorBox("Error", "Failed to start Python backend.");
-              app.quit();
-          }
-      } else {
-          dialog.showErrorBox("Error", "Python backend not found.");
-          app.quit();
+    app.on('window-all-closed', () => {
+      // Respect the OSX convention of having the application in memory even
+      // after all windows have been closed
+      if (process.platform !== 'darwin') {
+        app.quit();
       }
+    });
 
-      createWindow();
-      app.on('activate', () => {
-          // On macOS it's common to re-create a window in the app when the
-          // dock icon is clicked and there are no other windows open.
-          if (mainWindow === null) createWindow();
-      });
-
-      app.on('window-all-closed', () => {
-          pyproc?.kill();
-          app.quit();
-      });
+    app.on('before-quit', () => {
+      pyproc?.kill();
+    });
   })
   .catch(console.log);
